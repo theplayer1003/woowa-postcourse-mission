440eb4ab586d04dc75902c355568a33746344098
김준기 <theplayer1003@gimjungiui-Macmini.local>
2025-12-02 18:56:26 +0900

    Initial commit

---
+++ mission-4th/java-pairmatching-precourse/src/main/java/pairmatching/Application.java
@@ -0,0 +1,8 @@
+package pairmatching;
+
+public class Application {
+    public static void main(String[] args) {
+        // TODO 구현 진행
+        System.out.println("test");
+    }
+}
---
+++ mission-4th/java-vendingmachine-precourse/src/main/java/vendingmachine/Application.java
@@ -0,0 +1,8 @@
+package vendingmachine;
+
+public class Application {
+    public static void main(String[] args) {
+        // TODO: 프로그램 구현
+        System.out.println("test");
+    }
+}
