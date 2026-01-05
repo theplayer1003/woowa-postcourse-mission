7f9f378ccfd11b9e612d7ea59b2dbe6982f1fc19
김준기 <theplayer1003@MacBook-Pro.local>
2026-01-05 14:05:11 +0900

    feat: CsvReader 구현

--- mission-7th/java-attendance-7/src/main/java/attendance/Application.java
+++ mission-7th/java-attendance-7/src/main/java/attendance/Application.java
@@ -1,5 +1,8 @@
 package attendance;
 
+import camp.nextstep.edu.missionutils.DateTimes;
+import java.time.LocalDateTime;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
---
+++ mission-7th/java-attendance-7/src/main/java/attendance/global/util/CsvReader.java
@@ -0,0 +1,55 @@
+package attendance.global.util;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Function;
+
+public class CsvReader {
+    public <T> List<T> read(String fileName, Function<String, T> mapper, boolean skipHeader) {
+        final InputStream inputStream = getClass().getClassLoader().getResourceAsStream(fileName);
+
+        if (inputStream == null) {
+            throw new IllegalArgumentException("파일을 찾을 수 없습니다 " + fileName);
+        }
+
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8))) {
+            return generateList(reader, mapper, skipHeader);
+        } catch (IOException e) {
+            throw new IllegalStateException("읽기 중 오류 발생 " + fileName, e);
+        }
+    }
+
+    public <T> List<T> read(String fileName, Function<String, T> mapper) {
+        return read(fileName, mapper, true);
+    }
+
+    private <T> List<T> generateList(BufferedReader reader, Function<String, T> mapper, boolean skipHeader)
+            throws IOException {
+        List<T> results = new ArrayList<>();
+        String line;
+        boolean isFirstLine = true;
+
+        while ((line = reader.readLine()) != null) {
+            if (line.isBlank() || line.startsWith("#")) {
+                continue;
+            }
+
+            if (isFirstLine) {
+                isFirstLine = false;
+                if (skipHeader) {
+                    continue;
+                }
+            }
+
+            final T object = mapper.apply(line);
+            results.add(object);
+        }
+
+        return results;
+    }
+}
